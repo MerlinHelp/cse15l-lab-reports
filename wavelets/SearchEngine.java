807839e5dcdc0638548260bd80021b221e89546c
Richard Vo <richardvo2004@gmail.com>
2024-01-17 17:58:18 -0800

    Added multiple add, updated home screen

--- wavelets/SearchEngine.java
+++ wavelets/SearchEngine.java
@@ -1,32 +1,75 @@
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    ArrayList<String> strList = new ArrayList<>();
+    List<String> strList = new ArrayList<>();
 
-    public String handleRequest(URI url) {
-        if (url.getPath().equals("/")) {
-            return String.format("Home:\nAdd url path: '/add?s=<string>' to add to SearchEngine\nAdd url path: '/search?s=<string>' to search for strings containing <string>.");
-        } else if (url.getPath().contains("/add")) {
-            String[] parameters = url.getQuery().split("=");
-            if (parameters[0].equals("s")) {
-                strList.add(parameters[1]);
-                return String.format("Successfully added '%s' to the SearchEngine!", parameters[1]);
+    String homeString = "Home:\n";
+    String add = "\tAdd url path: '/add?s=<string>' to add to SearchEngine.\n";
+    String search = "\tAdd url path: '/search?s=<string>' to search for strings containing <string>.\n";
+    String multipleAddOrSearch = "\tUse \"\" to search multiple items when separated by commas. Strings without \"\" are considered single items.\n";
+    String multipleAddOrSearchExample = "\tEx: \"<string1>, <string2>\" takes BOTH '<string1>' and '<string2>'.\n \tEx 2: <string>, <string2> takes ONLY '<string>, <string2>.'";
+
+    public boolean checkQuotes(String fieldValue) {
+        return fieldValue.startsWith("\"") && fieldValue.endsWith("\"");
+    }
+
+    private void addToList(String fieldValue, boolean hasQuotes) {
+        if (hasQuotes) {
+            for (String s : fieldValue.substring(1, fieldValue.length() - 1).split(",")) {
+                strList.add(s.strip());
             }
-        } else if (url.getPath().contains("/search")) {
+        } else strList.add(fieldValue);
+    }
+
+    public String handleRequest(URI url) 
+    {
+        String urlPath = url.getPath();
+        
+        if (urlPath.equals("/")) {
+            return homeString + add + search + multipleAddOrSearch + multipleAddOrSearchExample;
+        } else if (urlPath.startsWith("/add")) {
+            String[] parameters = url.getQuery().split("=");
+
+            if (parameters[0] != null && parameters[0].equals("s")) {
+                boolean hasQuotes = checkQuotes(parameters[1]);
+                this.addToList(parameters[1], hasQuotes);
+
+                String addedString = "";
+                if (hasQuotes) {
+                    for (String s : parameters[1].substring(1, parameters[1].length() - 1).split(",")) {
+                        addedString += "'" + s.strip() + "', "; 
+                    }
+                    addedString = addedString.substring(0, addedString.length() - 2);
+                } else {
+                    addedString = "'" + parameters[1] + "'";
+                }
+
+                return String.format("Successfully added %s to the SearchEngine", addedString);
+            }
+        } else if (urlPath.startsWith("/search")) { // TODO: Implement double word search
             String[] parameters = url.getQuery().split("=");
             ArrayList<String> results = new ArrayList<>();
-            if (parameters[0].equals("s")) {
-                for (String s : strList) {
+            if (parameters[0] != null && parameters[0].equals("s")) {
+                for (String s : this.strList) {
                     if (s != null && s.contains(parameters[1])) {
                         results.add(s);
                     } 
                 }
                 return "Results: " + results.toString();
             }
+        } else if (urlPath.startsWith("/all")) {
+            return String.format("Entire search engine: %s", 
+                                String.join(", ", strList
+                                .stream()
+                                .map(str -> ("'" + str + "'"))
+                                .collect(Collectors.toList())
+                                ));
         }
         return "404 Not Found!";
     }
